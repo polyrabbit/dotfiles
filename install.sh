be76adaa1e28da87b87a5dc913711a3d28399e53
polyrabbit <mcx_221@foxmail.com>
2013-10-03 20:39:38 +0800

    Redefine the role of install and provision

--- install.sh
+++ install.sh
@@ -1,10 +1,69 @@
 #!/usr/bin/env bash
 
+error() {
+    echo -e "\e[31m$@\e[0m" 1>&2
+}
+
+info() {
+    echo -e "\e[32m$@\e[0m" 1>&2
+}
+
+is_success() {
+    if [ "$?" -gt 0 ]; then
+        error Something is wrong while installing $1
+        exit 1
+    fi
+}
+
+apt_install() {
+    if which "$1" &>/dev/null; then
+        info "$1" already exists
+    else
+        info Installing "$1"
+        apt-get install "$1" -y
+        is_success "$1"
+    fi
+}
+
+pip_install() {
+    info Installing "$1"
+    pip install "$1"
+    is_success "$1"
+}
+
+apt_pkgs=(
+git
+zsh
+python-pip
+tmux
+python-vte
+vim-gtk
+python-dev
+libevent-dev
+autojump
+libreoffice
+fcitx
+fcitx-table-wbpy
+ctags
+)
+python_pkgs=(
+virtualenv
+virtualenvwrapper
+gevent
+)
+
+if [ $UID -ne 0 ]; then
+    error 'Superuser privileges are required to run this script.'
+    exit 1
+fi
+
 cd `dirname $0`
-for file in $(find . -maxdepth 1 -name '*rc' -type f); do
-    cp -a $file $HOME
+
+for pkg in ${apt_pkgs[@]}; do
+    apt_install $pkg
 done
 
-if ! [ -d ~/.vim/bundle/vundle ]; then
-    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
+for pkg in ${python_pkgs[@]}; do
+    pip_install $pkg
+done
+
--- provision.sh
+++ provision.sh
@@ -8,74 +8,27 @@ info() {
     echo -e "\e[32m$@\e[0m" 1>&2
 }
 
-is_success() {
-    if [ "$?" -gt 0 ]; then
-        error Something is wrong while installing $1
-        exit 1
-    fi
-}
-
-apt_install() {
-    if which "$1" &>/dev/null; then
-        info "$1" already exists
-    else
-        info Installing "$1"
-        apt-get install "$1" -y
-        is_success "$1"
-    fi
-}
-
-pip_install() {
-    info Installing "$1"
-    pip install "$1"
-    is_success "$1"
-}
-
-apt_pkgs=(
-git
-zsh
-python-pip
-tmux
-python-vte
-vim-gtk
-python-dev
-libevent-dev
-autojump
-libreoffice
-fcitx
-fcitx-table-wbpy
-ctags
-)
-python_pkgs=(
-virtualenv
-virtualenvwrapper
-gevent
-)
-
-if [ $UID -ne 0 ]; then
-    error 'Superuser privileges are required to run this script.'
-    exit 1
+if [ $UID -eq 0 ]; then
+    error 'Why are you run as root?'
 fi
 
 cd `dirname $0`
 
-for pkg in ${apt_pkgs[@]}; do
-    apt_install $pkg
-done
-
-for pkg in ${python_pkgs[@]}; do
-    pip_install $pkg
+for file in $(find . -maxdepth 1 -name '*rc' -type f); do
+    info copying $file to $HOME
+    cp -a $file $HOME
 done
 
 # Append my pub-key
 if [ -f id_rsa.pub ]; then
     mkdir -p ~/.ssh
     pub_key=`cat id_rsa.pub`
-    if grep -q "$pub_key" ~/.ssh/authorized_keys; then
+    key_path=~/.ssh/authorized_keys
+    if [ -f $key_path ] && grep -q "$pub_key" $key_path; then
         info You\'re already the boss
     else
         info Appending public key
-        echo $pub_key >>~/.ssh/authorized_keys
+        echo $pub_key >>$key_path
     fi
 fi
 
@@ -85,5 +38,14 @@ else
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 fi
 
-info Changing poly\'s shell to zsh
-chsh -s /bin/zsh poly
+if [ ${SHELL:(-3)} != zsh ]; then
+    info Changing poly\'s shell to zsh
+    chsh -s /bin/zsh poly
+fi
+
+if [ -d ~/.vim/bundle/vundle ]; then
+    info vundle already exists
+else
+    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
