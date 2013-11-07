b061d4fb9c8c11eac083927ff128d75cf48dee2f
poly <mcx_221@foxmail.com>
2013-11-07 19:06:26 +0800

    As the previous

--- provision.sh
+++ provision.sh
@@ -43,18 +43,18 @@ if [ -f id_rsa.pub ]; then
 fi
 
 if [ -d ~/.oh-my-zsh ]; then
-    info oh-my-zsh already exists
+    info "oh-my-zsh already exists"
 else
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 fi
 
 if [ ${SHELL:(-3)} != zsh ]; then
-    info Changing poly\'s shell to zsh
+    info "Changing poly\'s shell to zsh"
     chsh -s /bin/zsh poly
 fi
 
 if [ -d ~/.vim/bundle/vundle ]; then
-    info vundle already exists
+    info "vundle already exists"
 else
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
